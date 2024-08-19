36014116ee396f4a0ed4d1bceb8df52b1705af8a
NOURHANABDALHLIME <nourhanabalhlime@gmail.com>
2024-08-19 14:00:56 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,18 @@
+/*var men = document.querySelector('#check');
+function open_cart(){
+    men.classList.add('active')
+}
+function close_cart(){
+    men.classList.remove('active')
+}
+*/
+
+
+
+var men = document.querySelector('#men');
+function open_cart(){
+    men.classList.add('active')
+}
+function close_cart(){
+    men.classList.remove('active')
+}
